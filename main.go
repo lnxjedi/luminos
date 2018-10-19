aa7fdc27cd47140cb5b02963760cc569a32f0f05
David Parsley <dlp7y@virginia.edu>
2018-10-19 15:11:04 -0400

    Open 0.9.3 devel

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ import (
 const pathSeparator = string(os.PathSeparator)
 
 // Version holds the software version.
-const Version = "v0.9.2"
+const Version = "v0.9.3-snapshot"
 
 // Commit of current build
 var Commit string
